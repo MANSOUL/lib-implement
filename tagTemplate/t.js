aa30700bab9df083786e2eb15906ab1f6186e04e
MANSOUL <xiaohu12347@gmail.com>
2019-09-03 17:03:06 +0800

    fix: 生成抽象语法树

--- tagTemplate/t.js
+++ tagTemplate/t.js
@@ -2,7 +2,7 @@ import parse from './lexical.js';
 import generate from './syntax.js';
 import { tValue } from './utils.js';
 
-function t (statics, ...values) {
+export function t (statics, ...values) {
   let html = '';
   statics.map((s, i) => {
     html += s + tValue(values[i]);
@@ -10,29 +10,6 @@ function t (statics, ...values) {
   return html;
 }
 
-function render () {
-  let name = 'Fruit List';
-  let fruits = ['Apple', 'Banana', 'Peach'];
-  let template =  
-  t`
-    <div class="block">
-      <ul class="list">
-        ${
-          fruits.map((f, index) => {
-            return t`<li class="list__item" data-index=${index}>
-                      ${
-                        f.split('').map(c => t`<span>${c}</span>`)
-                      }
-                    </li> `
-          })
-        }
-      </ul>  
-      <p class="block__desc">${name} ${null}</p>
-    </div>
-  `;
-  return template;
+export function ast (template) {
+  return generate(parse(template)).children[0];
 }
-
-console.log(render())
-console.log(parse(render()))
-console.log(generate(parse(render())))
